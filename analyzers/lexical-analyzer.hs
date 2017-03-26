93cf265bc025a89521e8685087597bc6ebad2454
Breno <brenomfviana@gmail.com>
2017-03-26 14:43:38 -0300

    Implements the tokenization of some commands in the lexical analyzer.

--- analyzers/lexical-analyzer.hs
+++ analyzers/lexical-analyzer.hs
@@ -8,12 +8,33 @@ import Data.List
 import Data.List.Split
 
 --
-token:: String -> [String]
-token str = split (onSublist "=") str
+equality:: String -> [String]
+equality str = split (onSublist "=") str
+
+--
+difference:: [String] -> [String]
+difference [] = []
+difference (head:tail) = split (onSublist "!=") head ++ difference tail
+
+--
+belongs:: [String] -> [String]
+belongs [] = []
+belongs (head:tail) = split (onSublist ":e") head ++ belongs tail
+
+--
+biggerThan:: [String] -> [String]
+biggerThan [] = []
+biggerThan (head:tail) = split (onSublist "<=") head ++ biggerThan tail
+
+--
+lessThan:: [String] -> [String]
+lessThan [] = []
+lessThan (head:tail) = split (onSublist ">=") head ++ lessThan tail
+
 
 -- Analyzes the code
 ---- Input : List with the words
 ---- Output: List with all lexemes of the program
 analyzer:: [String] -> [String]
 analyzer [] = []
-analyzer (head:tail) = token(head) ++ analyzer(tail)
+analyzer (head:tail) = lessThan(biggerThan(belongs(difference(equality(head))))) ++ analyzer(tail)
