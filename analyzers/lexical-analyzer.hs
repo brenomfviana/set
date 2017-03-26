9f9e84dceb0bb207aded066693460c9b19dabd58
Breno <brenomfviana@gmail.com>
2017-03-25 23:01:04 -0300

    Adds set code samples.

--- analyzers/lexical-analyzer.hs
+++ analyzers/lexical-analyzer.hs
@@ -1,19 +1,19 @@
 -- Lexical Analyzer
--- Version: 23/03/2017
+-- Version: 25/03/2017
 -- Author : Breno Viana
 module LexicalAnalyzer (analyzer) where
 
-	-- Imports
-	import Data.List.Split
-	import Data.List
+-- Imports
+import Data.List.Split
+import Data.List
 
-	-- 
-	token:: [String] -> [String]
-	token [] = []
+--
+token:: [String] -> [String]
+token [] = []
 
-	-- Analyzes the code
-	---- Input : List with the words
-	---- Output: List with all lexemes of the program
-	analyzer:: [String] -> [String]
-	analyzer [] = []
-	analyzer (head,tail) = token(head) ++ analyzer(tail)
+-- Analyzes the code
+---- Input : List with the words
+---- Output: List with all lexemes of the program
+analyzer:: [String] -> [String]
+analyzer [] = []
+analyzer (head,tail) = token(head) ++ analyzer(tail)
