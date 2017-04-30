6248b00b2e520b69634cfaa3b8bb2c0ef407d358
Breno Viana <brenomfviana@gmail.com>
2017-04-29 22:29:43 -0300

    Moves the folder syntax analyzer.

--- analyzers/syntax-analyzer/syntax-analyzer.hs
+++ analyzers/syntax-analyzer/syntax-analyzer.hs
@@ -1,14 +1,19 @@
+-- Syntax Analyzer
+-- Version: 25/03/2017
+-- Author :
+
+-- Imports
 import Data.List
 
 type Name = [Char]
 
 type Address = Int
 
-data Value = Int (Int) 
-            | String ([Char]) 
-            | Boolean (Bool) 
-            | Double (Double) 
-            | Float (Float) 
+data Value =  Int     (Int)
+            | String  ([Char])
+            | Boolean (Bool)
+            | Double  (Double)
+            | Float   (Float)
             | Pointer (Address) deriving (Show)
 
 type Escope = [Char]
