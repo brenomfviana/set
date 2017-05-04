c9c7668856b2b7e1a447e3c0e209a5786559a96a
Breno Viana <brenomfviana@gmail.com>
2017-05-03 22:12:48 -0300

    Adds symbol removal and update functions in the symbol table.

--- interpreter/SymbolTable.hs
+++ interpreter/SymbolTable.hs
@@ -6,6 +6,8 @@ module SymbolTable where
 
 -- Import
 import Parser
+import Data.List
+import Data.Tuple
 
 -- Variable name
 type Name = [Char]
@@ -31,11 +33,37 @@ addSymbol :: Symbol -> SymbolTable -> SymbolTable
 addSymbol (a, b, c) symbols = symbols ++ [(a, b, c)]
 
 
+-- Remove a symbol to symbol table
+removeSymbol :: SymbolTable -> Name -> Escope -> SymbolTable
+-- Error symbol not found
+removeSymbol [] _ _ = []
+-- Search and remove symbol
+removeSymbol (head:tail) name escope = let (n, _, e) = head in
+                                        if (name == n && escope == e) then removeSymbol tail
+                                        else head : findSymbol tail name escope
+
+
+-- Update a symbol to symbol table
+updateSymbol :: SymbolTable -> Name -> Escope -> Value -> SymbolTable
+-- Error symbol not found
+updateSymbol [] _ _ _ = []
+-- Search and update symbol
+updateSymbol (head:tail) name escope value = let (n, _, e) = head in
+                                        if (name == n && escope == e) then setValue head value : tail
+                                        else head : findSymbol tail name escope
+
+
+-- Set value
+setValue :: Symbol -> Value -> Symbol
+setValue (n, v, e) value = (n, value, e)
+
+
+
 -- Find symbol
 findSymbol :: SymbolTable -> Name -> Escope -> Symbol
--- Error variable not found
+-- Error symbol not found
 findSymbol [] _ _ = ("Not Found", "NF", "NULL")
--- Searching variable
+-- Search symbol
 findSymbol (head:tail) name escope = let (n, _, e) = head in
                                         if (name == n && escope == e) then head
-                                        else findSymbol tail name escope
+                                        else head : findSymbol tail name escope
