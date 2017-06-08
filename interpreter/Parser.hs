6ed13c2fd3bbb54f7dd0c2bb346dacbaa5c24743
Breno Viana <brenomfviana@gmail.com>
2017-06-08 20:53:18 -0300

    Updates parser: - Fixes assignment of compatible types.

--- interpreter/Parser.hs
+++ interpreter/Parser.hs
@@ -17,58 +17,61 @@ import SymbolTable
 -- - Program Token
 programToken = tokenPrim show updatePositon getToken where
     getToken (Program pos) = Just (Program pos)
-    getToken _       = Nothing
+    getToken _             = Nothing
 
 -- - End Token
 endToken = tokenPrim show updatePositon getToken where
     getToken (End pos) = Just (End pos)
-    getToken _   = Nothing
+    getToken _         = Nothing
 
 -- - ID Token
 idToken = tokenPrim show updatePositon getToken where
     getToken (Id x pos) = Just (Id x pos)
-    getToken _      = Nothing
+    getToken _          = Nothing
 
 -- - Colon Token
 colonToken = tokenPrim show updatePositon getToken where
     getToken (Colon pos) = Just (Colon pos)
-    getToken _     = Nothing
+    getToken _           = Nothing
 
 -- - Semicolon Token
-semiColonToken :: ParsecT [Token] st IO (Token)
+semiColonToken :: ParsecT [Token] st IO(Token)
 semiColonToken = tokenPrim show updatePositon getToken where
     getToken (SemiColon pos) = Just (SemiColon pos)
-    getToken _         = Nothing
+    getToken _               = Nothing
 
 -- - Assign Token
 assignToken = tokenPrim show updatePositon getToken where
     getToken (Assign pos) = Just (Assign pos)
-    getToken _      = Nothing
+    getToken _            = Nothing
 
 -- - Type Token
 typeToken = tokenPrim show updatePositon getToken where
     getToken (Type x pos) = Just (Type x pos)
-    getToken _        = Nothing
+    getToken _            = Nothing
 
 -- - Nat Token
 natToken = tokenPrim show updatePositon getToken where
     getToken (Nat x pos) = Just (Nat x pos)
-    getToken _       = Nothing
+    getToken _           = Nothing
 
 -- - Int Token
 intToken = tokenPrim show updatePositon getToken where
+    getToken (Nat x pos) = Just (Int x pos)
     getToken (Int x pos) = Just (Int x pos)
-    getToken _       = Nothing
+    getToken _           = Nothing
 
 -- - Real Token
 realToken = tokenPrim show updatePositon getToken where
+    getToken (Nat  x pos) = let y = integerToFloat(x) in Just (Real y pos)
+    getToken (Int  x pos) = let y = integerToFloat(x) in Just (Real y pos)
     getToken (Real x pos) = Just (Real x pos)
-    getToken _         = Nothing
+    getToken _            = Nothing
 
 -- - Bool Token
 boolToken = tokenPrim show updatePositon getToken where
     getToken (Bool x pos) = Just (Bool x pos)
-    getToken _         = Nothing
+    getToken _            = Nothing
 
 -- - Univ Token
 -- univToken = tokenPrim show updatePositon getToken where
@@ -78,46 +81,54 @@ boolToken = tokenPrim show updatePositon getToken where
 -- - Text Token
 textToken = tokenPrim show updatePositon getToken where
     getToken (Text x pos) = Just (Text x pos)
-    getToken _        = Nothing
+    getToken _            = Nothing
 
 -- - Addition Token
 additionToken = tokenPrim show updatePositon getToken where
     getToken (Addition p) = Just (Addition p)
-    getToken _       = Nothing
+    getToken _            = Nothing
 
 -- - Subtraction Token
 subtractionToken = tokenPrim show updatePositon getToken where
     getToken (Subtraction p) = Just (Subtraction p)
-    getToken _       = Nothing
+    getToken _               = Nothing
 
 -- - Multiplication Token
 multiplicationToken = tokenPrim show updatePositon getToken where
     getToken (Multiplication p) = Just (Multiplication p)
-    getToken _       = Nothing
-    
+    getToken _                  = Nothing
+
 -- - Open_Parentheses Token
 open_Parentheses = tokenPrim show updatePositon getToken where
     getToken (Open_Parentheses p) = Just (Open_Parentheses p)
-    getToken _       = Nothing
-    
--- - Close_Parentheses Token
-close_Parentheses = tokenPrim show updatePositon getToken where
-    getToken (Close_Parentheses p) = Just (Close_Parentheses p)
-    getToken _       = Nothing
+    getToken _                    = Nothing
 
 -- - Division Token
 -- divisionToken = tokenPrim show updatePositon getToken where
 --     getToken (Division p) = Just (Division p)
 --     getToken _       = Nothing
 
+-- - Close_Parentheses Token
+close_Parentheses = tokenPrim show updatePositon getToken where
+    getToken (Close_Parentheses p) = Just (Close_Parentheses p)
+    getToken _                     = Nothing
+
 -- - Update position
 -- SourcePos Position
--- Token     --
--- [Token]   --
+-- Token     Token
+-- [Token]   All tokens
 -- Return    Updated position
 updatePositon :: SourcePos -> Token -> [Token] -> SourcePos
 updatePositon position _ (token:_) = position -- necessita melhoria
 updatePositon position _ []        = position
+
+-- - Convert int in float
+-- Int    Integer value
+-- Return Float value
+integerToFloat :: Int -> Float
+integerToFloat i = do
+    let r = fromIntegral(toInteger i) / fromIntegral 1 in r
+
 
 
 -- -----------------------------------------------------------------------------
@@ -125,7 +136,7 @@ updatePositon position _ []        = position
 -- -----------------------------------------------------------------------------
 
 -- - Program
-program :: ParsecT [Token] [(Token, Token)] IO ([Token])
+program :: ParsecT [Token] [(Token, Token)] IO([Token])
 program = do
     a <- programToken
     b <- idToken
@@ -137,9 +148,9 @@ program = do
     return (a:[b] ++ c ++ d ++ e:[f])
 
 -- - Variable declarations
--- ParsecT          --
--- [Token]          --
--- [(Token, Token)] --
+-- ParsecT          ParsecT
+-- [Token]          Token list
+-- [(Token, Token)] State
 varDecls :: ParsecT [Token] [(Token, Token)] IO([Token])
 varDecls = do
     first <- varDecl
@@ -147,9 +158,9 @@ varDecls = do
     return (first ++ next)
 
 -- - Variable declaration
--- ParsecT          --
--- [Token]          --
--- [(Token, Token)] --
+-- ParsecT          ParsecT
+-- [Token]          Token list
+-- [(Token, Token)] State
 varDecl :: ParsecT [Token] [(Token, Token)] IO([Token])
 varDecl = do
     a <- typeToken
@@ -162,17 +173,17 @@ varDecl = do
     return (a:b:[c])
 
 -- - Variable declaration remaining
--- ParsecT          --
--- [Token]          --
--- [(Token, Token)] --
+-- ParsecT          ParsecT
+-- [Token]          Token list
+-- [(Token, Token)] State
 remainingVarDecls :: ParsecT [Token] [(Token, Token)] IO([Token])
 remainingVarDecls = (do a <- varDecls
                         return (a)) <|> (return [])
 
 -- - Statements
--- ParsecT          --
--- [Token]          --
--- [(Token, Token)] --
+-- ParsecT          ParsecT
+-- [Token]          Token list
+-- [(Token, Token)] State
 stmts :: ParsecT [Token] [(Token, Token)] IO([Token])
 stmts = do
     first <- assign
@@ -180,17 +191,17 @@ stmts = do
     return (first ++ next)
 
 -- - Statements remaining
--- ParsecT          --
--- [Token]          --
--- [(Token, Token)] --
+-- ParsecT          ParsecT
+-- [Token]          Token list
+-- [(Token, Token)] State
 remainingStmts :: ParsecT [Token] [(Token, Token)] IO([Token])
 remainingStmts = (do a <- stmts
                      return (a)) <|> (return [])
 
--- - Assing
--- ParsecT          --
--- [Token]          --
--- [(Token, Token)] --
+-- - Assign
+-- ParsecT          ParsecT
+-- [Token]          Token list
+-- [(Token, Token)] State
 assign :: ParsecT [Token] [(Token, Token)] IO([Token])
 assign = do
     a <- idToken
@@ -198,14 +209,14 @@ assign = do
     c <- expression
     d <- semiColonToken
     s <- getState
-    --
+    -- Check if the types are compatible
     if (not (compatible (getType a s) c)) then fail "Type mismatch."
     else
-      do
-        updateState(symtableUpdate(a, c))
-        s <- getState
-        liftIO (print s)
-        return (a:b:[c])
+        do
+            updateState(symtableUpdate(a, (updateType (getType a s) c)))
+            s <- getState
+            liftIO (print s)
+            return (a:b:[c])
 
 
 -- -----------------------------------------------------------------------------
@@ -216,8 +227,8 @@ assign = do
 -- Type   Variable type
 -- Return Initial variable value
 getDefaultValue :: Token -> Token
-getDefaultValue (Type "Nat" pos) = Nat 0 pos
-getDefaultValue (Type "Int" pos) = Int 0 pos
+getDefaultValue (Type "Nat"  pos) = Nat 0 pos
+getDefaultValue (Type "Int"  pos) = Int 0 pos
 getDefaultValue (Type "Real" pos) = Real 0.0 pos
 getDefaultValue (Type "Bool" pos) = Bool False pos
 -- getDefaultValue (Type "Univ" pos) = Univ "" pos
@@ -226,22 +237,35 @@ getDefaultValue (Type "Text" pos) = Text "" pos
 -- getDefaultValue (Type "Set[" <type> "]") = "Set[" <type> "]" "\empty"
 
 -- - Get type
--- [Token]          --
--- [(Token, Token)] --
+-- [Token]          Token list
+-- [(Token, Token)] State
 -- Return           Variable type
 getType :: Token -> [(Token, Token)] -> Token
-getType _ [] = error "Variable not found"
+getType _ [] = error "Variable not found."
 getType (Id id1 p1) ((Id id2 _, value):t) = if id1 == id2 then value
                                             else getType (Id id1 p1) t
 
+-- Cast
+updateType :: Token -> Token -> Token
+updateType (Nat _ _)   (Nat i p) = Nat i p
+updateType (Int _ _)   (Nat i p) = Int i p
+updateType (Int _ _)   (Int i p) = Int i p
+updateType (Real _ _)  (Nat i p) = let x = integerToFloat(i) in Real x p
+updateType (Real _ _)  (Int i p) = let x = integerToFloat(i) in Real x p
+updateType (Real _ _) (Real i p) = Real i p
+updateType _ _ = error "Invalid cast."
+
 -- - Check whether types are compatible
--- ParsecT          --
--- [Token]          --
--- [(Token, Token)] --
+-- ParsecT          ParsecT
+-- [Token]          Token list
+-- [(Token, Token)] State
 compatible :: Token -> Token -> Bool
 compatible (Nat _ _)   (Nat _ _) = True
 compatible (Int _ _)   (Int _ _) = True
+compatible (Int _ _)   (Nat _ _) = True
 compatible (Real _ _) (Real _ _) = True
+compatible (Real _ _)  (Int _ _) = True
+compatible (Real _ _)  (Nat _ _) = True
 compatible (Bool _ _) (Bool _ _) = True
 -- compatible (Univ _ _) (Univ _ _) = True
 compatible (Text _ _) (Text _ _) = True
@@ -255,53 +279,53 @@ compatible _ _ = False
 -- -----------------------------------------------------------------------------
 
 -- - Expressions
--- ParsecT          --
--- [Token]          --
--- [(Token, Token)] --
+-- ParsecT          ParsecT
+-- [Token]          Token list
+-- [(Token, Token)] State
 expression :: ParsecT [Token] [(Token,Token)] IO(Token)
-expression = try  binaryExpression <|> unaryExpression <|> parentExpression
- 
+expression = try binaryExpression <|> unaryExpression <|> parentExpression
+
 
 -- - Unary expression
--- ParsecT          --
--- [Token]          --
--- [(Token, Token)] --
+-- ParsecT          ParsecT
+-- [Token]          Token list
+-- [(Token, Token)] State
 unaryExpression :: ParsecT [Token] [(Token,Token)] IO(Token)
 unaryExpression = do
-                   a <- natToken <|> intToken <|> realToken <|> boolToken
+                    a <- natToken <|> intToken <|> realToken <|> boolToken
                         <|> textToken
-                   return (a)
-   
+                    return (a)
+
 
 -- - Unary expression
--- ParsecT          --
--- [Token]          --
--- [(Token, Token)] --               
+-- ParsecT          ParsecT
+-- [Token]          Token list
+-- [(Token, Token)] State
 parentExpression :: ParsecT [Token] [(Token,Token)] IO(Token)
 parentExpression = do
                   a <- open_Parentheses
                   b <- expression <|> parentExpression
                   c <- close_Parentheses
-                  return (b)                   
+                  return (b)
 
 -- - Binary expression
--- ParsecT          --
--- [Token]          --
--- [(Token, Token)] --
+-- ParsecT          ParsecT
+-- [Token]          Token list
+-- [(Token, Token)] State
 binaryExpression :: ParsecT [Token] [(Token,Token)] IO(Token)
 binaryExpression = do
                    a <- natToken <|> intToken <|> realToken <|> parentExpression
                    b <- additionToken <|> subtractionToken <|> multiplicationToken
-                   c <- expression 
+                   c <- expression
 
                    return (eval a b c)
-                   
+
 -- - Evaluation
--- ParsecT          --
--- [Token]          --
--- [(Token, Token)] --
+-- ParsecT          ParsecT
+-- [Token]          Token list
+-- [(Token, Token)] State
 eval :: Token -> Token -> Token -> Token
-eval (Nat x p)  (Addition _)       (Nat y _)  = Nat (x + y) p
+eval (Nat x p)  (Addition _)       (Nat y _)  = Nat  (x + y) p
 eval (Nat x p)  (Addition _)       (Int y _)  = Int  (x + y) p
 eval (Int x p)  (Addition _)       (Nat y _)  = Int  (x + y) p
 eval (Int x p)  (Addition _)       (Int y _)  = Int  (x + y) p
@@ -323,7 +347,7 @@ eval (Real x p) (Multiplication _) (Real y _) = Real (x * y) p
 -- -----------------------------------------------------------------------------
 
 -- - Parser
--- [Token]          --
+-- [Token]          Token list
 parser :: [Token] -> IO (Either ParseError [Token])
 parser tokens = runParserT program [] "Error message" tokens
 
