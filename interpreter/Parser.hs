9e8f35f6408081b5265245b46746310267a5b668
Felipe Barbalho <felipebarbalho.95@gmail.com>
2017-06-08 22:14:45 -0300

    Implements more boolean expressions

--- interpreter/Parser.hs
+++ interpreter/Parser.hs
@@ -98,15 +98,42 @@ multiplicationToken = tokenPrim show updatePositon getToken where
     getToken (Multiplication p) = Just (Multiplication p)
     getToken _                  = Nothing
 
--- - Open_Parentheses Token
-open_Parentheses = tokenPrim show updatePositon getToken where
-    getToken (Open_Parentheses p) = Just (Open_Parentheses p)
-    getToken _                    = Nothing
-
 -- - Division Token
 -- divisionToken = tokenPrim show updatePositon getToken where
 --     getToken (Division p) = Just (Division p)
 --     getToken _       = Nothing
+
+
+-- - Equality Token
+equalityToken = tokenPrim show updatePositon getToken where
+    getToken (Equality p) = Just (Equality p)
+    getToken _            = Nothing
+
+-- - Greater Token
+greaterToken = tokenPrim show updatePositon getToken where
+    getToken (Greater p) = Just (Greater p)
+    getToken _           = Nothing
+
+-- - GreaterOrEqual Token
+greaterOrEqualToken = tokenPrim show updatePositon getToken where
+    getToken (GreaterOrEqual p) = Just (GreaterOrEqual p)
+    getToken _                  = Nothing
+    
+-- - Smaller Token
+smallerToken = tokenPrim show updatePositon getToken where
+    getToken (Smaller p) = Just (Smaller p)
+    getToken _           = Nothing
+
+-- - SmallerOrEqual Token
+smallerOrEqualToken = tokenPrim show updatePositon getToken where
+    getToken (SmallerOrEqual p) = Just (SmallerOrEqual p)
+    getToken _                  = Nothing
+
+
+-- - Open_Parentheses Token
+open_Parentheses = tokenPrim show updatePositon getToken where
+    getToken (Open_Parentheses p) = Just (Open_Parentheses p)
+    getToken _                    = Nothing
 
 -- - Close_Parentheses Token
 close_Parentheses = tokenPrim show updatePositon getToken where
@@ -285,8 +312,7 @@ compatible _ _ = False
 -- [Token]          Token list
 -- [(Token, Token)] State
 expression :: ParsecT [Token] [(Token,Token)] IO(Token)
-expression = try binaryExpression <|> unaryExpression <|> parentExpression
-
+expression = try  binaryExpression <|> parentExpression <|> unaryExpression
 
 -- - Unary expression
 -- ParsecT          ParsecT
@@ -294,21 +320,36 @@ expression = try binaryExpression <|> unaryExpression <|> parentExpression
 -- [(Token, Token)] State
 unaryExpression :: ParsecT [Token] [(Token,Token)] IO(Token)
 unaryExpression = do
-                    a <- natToken <|> intToken <|> realToken <|> boolToken
-                        <|> textToken
-                    return (a)
+                   a <- natToken <|> intToken <|> realToken <|> boolToken <|> textToken
+                   return (a)
 
 
--- - Unary expression
+-- - Boolean Operations
 -- ParsecT          ParsecT
 -- [Token]          Token list
 -- [(Token, Token)] State
+booleanOP :: ParsecT [Token] [(Token,Token)] IO(Token)
+booleanOP = equalityToken <|> greaterToken <|> greaterOrEqualToken <|> smallerToken <|> smallerOrEqualToken
+
+
+-- - Aritimetc Operations
+-- ParsecT          ParsecT
+-- [Token]          Token list
+-- [(Token, Token)] State
+numberOP :: ParsecT [Token] [(Token,Token)] IO(Token)
+numberOP = additionToken <|> subtractionToken <|> multiplicationToken
+
+
+-- - Expression with parentheses
+-- ParsecT          ParsecT
+-- [Token]          Token list
+-- [(Token, Token)] State             
 parentExpression :: ParsecT [Token] [(Token,Token)] IO(Token)
 parentExpression = do
                   a <- open_Parentheses
                   b <- expression <|> parentExpression
                   c <- close_Parentheses
-                  return (b)
+                  return (b)     
 
 -- - Binary expression
 -- ParsecT          ParsecT
@@ -316,10 +357,9 @@ parentExpression = do
 -- [(Token, Token)] State
 binaryExpression :: ParsecT [Token] [(Token,Token)] IO(Token)
 binaryExpression = do
-                   a <- natToken <|> intToken <|> realToken <|> parentExpression
-                   b <- additionToken <|> subtractionToken <|> multiplicationToken
-                   c <- expression
-
+                   a <- natToken <|> intToken <|> realToken <|> parentExpression <|> boolToken <|> textToken
+                   b <- numberOP <|> booleanOP
+                   c <- expression 
                    return (eval a b c)
 
 -- - Evaluation
@@ -338,6 +378,27 @@ eval (Real x p) (Subtraction _)    (Real y _) = Real (x - y) p
 eval (Nat x p)  (Multiplication _) (Nat y _)  = Nat  (x * y) p
 eval (Int x p)  (Multiplication _) (Int y _)  = Int  (x * y) p
 eval (Real x p) (Multiplication _) (Real y _) = Real (x * y) p
+
+eval (Nat x p) (Equality _) (Nat y _) = Bool (x == y) p
+eval (Int x p) (Equality _) (Int y _) = Bool (x == y) p
+eval (Real x p) (Equality _) (Real y _) = Bool (x == y) p
+eval (Text x p) (Equality _) (Text y _) = Bool (x == y) p
+
+eval (Nat x p) (Greater _) (Nat y _) = Bool (x > y) p
+eval (Int x p) (Greater _) (Int y _) = Bool (x > y) p
+eval (Real x p) (Greater _) (Real y _) = Bool (x > y) p
+
+eval (Nat x p) (GreaterOrEqual _) (Nat y _) = Bool (x >= y) p
+eval (Int x p) (GreaterOrEqual _) (Int y _) = Bool (x >= y) p
+eval (Real x p) (GreaterOrEqual _) (Real y _) = Bool (x >= y) p
+
+eval (Nat x p) (Smaller _) (Nat y _) = Bool (x < y) p
+eval (Int x p) (Smaller _) (Int y _) = Bool (x < y) p
+eval (Real x p) (Smaller _) (Real y _) = Bool (x < y) p
+
+eval (Nat x p) (SmallerOrEqual _) (Nat y _) = Bool (x <= y) p
+eval (Int x p) (SmallerOrEqual _) (Int y _) = Bool (x <= y) p
+eval (Real x p) (SmallerOrEqual _) (Real y _) = Bool (x <= y) p
 -- eval (Nat x p) (Division _) (Nat y _) = Nat (x / y) p
 -- eval (Int x p) (Division _) (Int y _) = Int (x / y) p
 -- eval (Real x p) (Division _) (Real y _) = Real (x / y) p
