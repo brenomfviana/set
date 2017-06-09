08715234d9747257a1f956e44f81d80a433f49da
Breno Viana <brenomfviana@gmail.com>
2017-06-09 08:20:08 -0300

    Updates set.hs, now it's possible to interpret any set file without recompiling the project.

--- interpreter/set.hs
+++ interpreter/set.hs
@@ -1,18 +1,36 @@
--- Interpreter
+-- Main
 -- Version: 09/06/2017
 module Main (main) where
 
 -- Imports
 import Control.Monad.IO.Class
+import System.Environment
 import System.IO.Unsafe
 import Interpreter
 import Lexer
 
--- Main
-main :: IO ()
-main = do
-    case unsafePerformIO (parser (getTokens "TestFiles/test-i.set")) of
+-- - Run
+run :: String -> IO()
+run "" = error "ERROR"
+run command = do
+    case unsafePerformIO (parser (getTokens command)) of
     {
         Left err -> print err;
-        Right ans -> print "The program ran successfully!" -- ans
+        Right ans -> putStrLn "The program ran successfully!" -- print ans
     }
+
+-- - Main
+main :: IO()
+main = do
+    -- Get the first argument
+    (command:args) <- getArgs
+    -- Check if there's no argument
+    if command == [] then
+        putStrLn "ERROR: No arguments."
+    else
+        -- Check for more than one argument
+        if length(args) >= 1 then do
+            putStrLn "WARNING: Too many arguments, just the first will be used."
+            run command
+        else
+            run command
