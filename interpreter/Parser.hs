0a402f7bb6d2b54fd9e2288a304cb21467415a83
Breno Viana <brenomfviana@gmail.com>
2017-06-10 21:41:45 -0300

    Updates the signature of the functions..

--- interpreter/Parser.hs
+++ interpreter/Parser.hs
@@ -2,48 +2,50 @@
 -- Version: 09/06/2017
 module Parser where
 
--- Imports
+-- External imports
 import Control.Monad.IO.Class
 import Text.Parsec
+-- Internal imports
 import Lexer
 import Types
+import State
 
 -- -----------------------------------------------------------------------------
 -- Parser to Tokens
 -- -----------------------------------------------------------------------------
 
 -- - Program Token
-programToken :: ParsecT [Token] st IO(Token)
+programToken :: ParsecT [Token] (Scope, [Var], [Statement]) IO(Token)
 programToken = tokenPrim show updatePositon getToken where
     getToken (Program pos) = Just (Program pos)
     getToken _             = Nothing
 
 -- - End Token
-endToken :: ParsecT [Token] st IO(Token)
+endToken :: ParsecT [Token] (Scope, [Var], [Statement]) IO(Token)
 endToken = tokenPrim show updatePositon getToken where
     getToken (End pos) = Just (End pos)
     getToken _         = Nothing
 
 -- - ID Token
-idToken :: ParsecT [Token] st IO(Token)
+idToken :: ParsecT [Token] (Scope, [Var], [Statement]) IO(Token)
 idToken = tokenPrim show updatePositon getToken where
     getToken (Id x pos) = Just (Id x pos)
     getToken _          = Nothing
 
 -- - Colon Token
-colonToken :: ParsecT [Token] st IO(Token)
+colonToken :: ParsecT [Token] (Scope, [Var], [Statement]) IO(Token)
 colonToken = tokenPrim show updatePositon getToken where
     getToken (Colon pos) = Just (Colon pos)
     getToken _           = Nothing
 
 -- - Semicolon Token
-semiColonToken :: ParsecT [Token] st IO(Token)
+semiColonToken :: ParsecT [Token] (Scope, [Var], [Statement]) IO(Token)
 semiColonToken = tokenPrim show updatePositon getToken where
     getToken (SemiColon pos) = Just (SemiColon pos)
     getToken _               = Nothing
 
 -- - Assign Token
-assignToken :: ParsecT [Token] st IO(Token)
+assignToken :: ParsecT [Token] (Scope, [Var], [Statement]) IO(Token)
 assignToken = tokenPrim show updatePositon getToken where
     getToken (Assign pos) = Just (Assign pos)
     getToken _            = Nothing
@@ -55,27 +57,27 @@ assignToken = tokenPrim show updatePositon getToken where
 -- --------------------------------------------------------
 
 -- - Type Token
-typeToken :: ParsecT [Token] st IO(Token)
+typeToken :: ParsecT [Token] (Scope, [Var], [Statement]) IO(Token)
 typeToken = tokenPrim show updatePositon getToken where
     getToken (Type x pos) = Just (Type x pos)
     getToken _            = Nothing
 
 -- - Nat Token
-natToken :: ParsecT [Token] st IO(Token)
+natToken :: ParsecT [Token] (Scope, [Var], [Statement]) IO(Token)
 natToken = tokenPrim show updatePositon getToken where
     getToken (Nat x pos) = if x < 0 then error "Invalid assignment."
                            else Just (Nat x pos)
     getToken _           = Nothing
 
 -- - Int Token
-intToken :: ParsecT [Token] st IO(Token)
+intToken :: ParsecT [Token] (Scope, [Var], [Statement]) IO(Token)
 intToken = tokenPrim show updatePositon getToken where
     getToken (Nat x pos) = Just (Int x pos)
     getToken (Int x pos) = Just (Int x pos)
     getToken _           = Nothing
 
 -- - Real Token
-realToken :: ParsecT [Token] st IO(Token)
+realToken :: ParsecT [Token] (Scope, [Var], [Statement]) IO(Token)
 realToken = tokenPrim show updatePositon getToken where
     getToken (Nat  x pos) = let y = integerToFloat(x) in Just (Real y pos)
     getToken (Int  x pos) = let y = integerToFloat(x) in Just (Real y pos)
@@ -83,7 +85,7 @@ realToken = tokenPrim show updatePositon getToken where
     getToken _            = Nothing
 
 -- - Bool Token
-boolToken :: ParsecT [Token] st IO(Token)
+boolToken :: ParsecT [Token] (Scope, [Var], [Statement]) IO(Token)
 boolToken = tokenPrim show updatePositon getToken where
     getToken (Bool x pos) = Just (Bool x pos)
     getToken _            = Nothing
@@ -94,9 +96,9 @@ boolToken = tokenPrim show updatePositon getToken where
 --     getToken _        = Nothing
 
 -- - Text Token
-textToken :: ParsecT [Token] st IO(Token)
+textToken :: ParsecT [Token] (Scope, [Var], [Statement]) IO(Token)
 textToken = tokenPrim show updatePositon getToken where
-    getToken (Text x pos) = Just (Text x pos)
+    getToken (Text x pos) = let y = removeQuote x in Just (Text y pos)
     getToken _            = Nothing
 
 
@@ -106,55 +108,55 @@ textToken = tokenPrim show updatePositon getToken where
 -- --------------------------------------------------------
 
 -- - Addition Token
-additionToken :: ParsecT [Token] st IO(Token)
+additionToken :: ParsecT [Token] (Scope, [Var], [Statement]) IO(Token)
 additionToken = tokenPrim show updatePositon getToken where
     getToken (Addition p) = Just (Addition p)
     getToken _            = Nothing
 
 -- - Subtraction Token
-subtractionToken :: ParsecT [Token] st IO(Token)
+subtractionToken :: ParsecT [Token] (Scope, [Var], [Statement]) IO(Token)
 subtractionToken = tokenPrim show updatePositon getToken where
     getToken (Subtraction p) = Just (Subtraction p)
     getToken _               = Nothing
 
 -- - Multiplication Token
-multiplicationToken :: ParsecT [Token] st IO(Token)
+multiplicationToken :: ParsecT [Token] (Scope, [Var], [Statement]) IO(Token)
 multiplicationToken = tokenPrim show updatePositon getToken where
     getToken (Multiplication p) = Just (Multiplication p)
     getToken _                  = Nothing
 
 -- - Division Token
-divisionToken :: ParsecT [Token] st IO(Token)
+divisionToken :: ParsecT [Token] (Scope, [Var], [Statement]) IO(Token)
 divisionToken = tokenPrim show updatePositon getToken where
     getToken (Division p) = Just (Division p)
     getToken _       = Nothing
 
 -- - Equality Token
-equalityToken :: ParsecT [Token] st IO(Token)
+equalityToken :: ParsecT [Token] (Scope, [Var], [Statement]) IO(Token)
 equalityToken = tokenPrim show updatePositon getToken where
     getToken (Equality p) = Just (Equality p)
     getToken _            = Nothing
 
 -- - Greater Token
-greaterToken :: ParsecT [Token] st IO(Token)
+greaterToken :: ParsecT [Token] (Scope, [Var], [Statement]) IO(Token)
 greaterToken = tokenPrim show updatePositon getToken where
     getToken (Greater p) = Just (Greater p)
     getToken _           = Nothing
 
 -- - GreaterOrEqual Token
-greaterOrEqualToken :: ParsecT [Token] st IO(Token)
+greaterOrEqualToken :: ParsecT [Token] (Scope, [Var], [Statement]) IO(Token)
 greaterOrEqualToken = tokenPrim show updatePositon getToken where
     getToken (GreaterOrEqual p) = Just (GreaterOrEqual p)
     getToken _                  = Nothing
 
 -- - Smaller Token
-smallerToken :: ParsecT [Token] st IO(Token)
+smallerToken :: ParsecT [Token] (Scope, [Var], [Statement]) IO(Token)
 smallerToken = tokenPrim show updatePositon getToken where
     getToken (Smaller p) = Just (Smaller p)
     getToken _           = Nothing
 
 -- - SmallerOrEqual Token
-smallerOrEqualToken :: ParsecT [Token] st IO(Token)
+smallerOrEqualToken :: ParsecT [Token] (Scope, [Var], [Statement]) IO(Token)
 smallerOrEqualToken = tokenPrim show updatePositon getToken where
     getToken (SmallerOrEqual p) = Just (SmallerOrEqual p)
     getToken _                  = Nothing
@@ -166,31 +168,31 @@ smallerOrEqualToken = tokenPrim show updatePositon getToken where
 -- --------------------------------------------------------
 
 -- - Print Token
-printToken :: ParsecT [Token] st IO(Token)
+printToken :: ParsecT [Token] (Scope, [Var], [Statement]) IO(Token)
 printToken = tokenPrim show updatePositon getToken where
     getToken (Print p) = Just (Print p)
     getToken _         = Nothing
 
 -- - If Token
-ifToken :: ParsecT [Token] st IO(Token)
+ifToken :: ParsecT [Token] (Scope, [Var], [Statement]) IO(Token)
 ifToken = tokenPrim show updatePositon getToken where
     getToken (If p) = Just (If p)
     getToken _      = Nothing
 
 -- - Else Token
-elseToken :: ParsecT [Token] st IO(Token)
+elseToken :: ParsecT [Token] (Scope, [Var], [Statement]) IO(Token)
 elseToken = tokenPrim show updatePositon getToken where
     getToken (Else p) = Just (Else p)
     getToken _        = Nothing
 
 -- - Else_If Token
-elseIfToken :: ParsecT [Token] st IO(Token)
+elseIfToken :: ParsecT [Token] (Scope, [Var], [Statement]) IO(Token)
 elseIfToken = tokenPrim show updatePositon getToken where
     getToken (Else_If p) = Just (Else_If p)
     getToken _           = Nothing
 
 -- - End_If Token
-endIfToken :: ParsecT [Token] st IO(Token)
+endIfToken :: ParsecT [Token] (Scope, [Var], [Statement]) IO(Token)
 endIfToken = tokenPrim show updatePositon getToken where
     getToken (End_If p) = Just (End_If p)
     getToken _          = Nothing
@@ -202,16 +204,22 @@ endIfToken = tokenPrim show updatePositon getToken where
 -- --------------------------------------------------------
 
 -- - OpenParentheses Token
-openParenthesesToken :: ParsecT [Token] st IO(Token)
+openParenthesesToken :: ParsecT [Token] (Scope, [Var], [Statement]) IO(Token)
 openParenthesesToken = tokenPrim show updatePositon getToken where
     getToken (Open_Parentheses p) = Just (Open_Parentheses p)
     getToken _                    = Nothing
 
 -- - CloseParentheses Token
-closeParenthesesToken :: ParsecT [Token] st IO(Token)
+closeParenthesesToken :: ParsecT [Token] (Scope, [Var], [Statement]) IO(Token)
 closeParenthesesToken = tokenPrim show updatePositon getToken where
     getToken (Close_Parentheses p) = Just (Close_Parentheses p)
     getToken _                     = Nothing
+
+-- - CloseParentheses Token
+ignoreToken :: ParsecT [Token] (Scope, [Var], [Statement]) IO(Token)
+ignoreToken = tokenPrim show updatePositon getToken where
+    getToken t = Just t
+    getToken _ = Nothing
 
 
 
